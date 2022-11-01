f7fb00580d0a1c9a71925b15e0b7dbeb6fd7883a
Kal O <koestreicher4@gmail.com>
2022-11-01 12:24:25 -0500

    get routes

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { notes } = require('./db/db.json'); 
+const { notes } = require('./db/db.json');
 
 const PORT = process.env.PORT || 3001;
 const app = express(); 
@@ -7,19 +7,26 @@ const fs = require('fs');
 const path = require('path'); 
 
 
+app.use(express.static('public'));
 
 
+app.post('/api/notes', (req, res) => {
+ 
+})
 
 
-
-app.get('/api/db', (req, res) => {
+app.get('/api/notes', (req, res) => {
   res.json(notes);
-})
+});
 
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
 
-app.get('/', (req, res) => {
+// get the homepage for the server to display INDEX.HTML
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
-})
+});
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);

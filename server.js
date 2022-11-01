eff53a5e62b4768ee3c76ea299003c7579eefa21
Kal O <koestreicher4@gmail.com>
2022-11-01 13:13:27 -0500

    post id

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
-const express = require('express');
-const { notes } = require('./db/db.json');
+const express = require('express');// require JSON file
 
 const PORT = process.env.PORT || 3001;
-const app = express(); 
-const fs = require('fs'); 
 const path = require('path'); 
-
+const fs = require('fs'); 
+const app = express(); 
 
 app.use(express.static('public'));
 
+app.use(express.urlencoded({ extended: true })); 
 
-app.post('/api/notes', (req, res) => {
- 
-})
+app.use(express.json()); 
+
+
+const addNewNote = (body, notesArr) => {
+  console.log(body, "body in NEW NOTE FUNC");
+  console.log(notesArr, "DB / notes array");
+}
+
 
 
 app.get('/api/notes', (req, res) => {
@@ -20,13 +24,32 @@ app.get('/api/notes', (req, res) => {
 });
 
 app.get('/notes', (req, res) => {
+  
   res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
-// get the homepage for the server to display INDEX.HTML
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
+
+
+app.post('/api/notes', (req, res) => {
+  const { title, text } = req.body;
+  console.log({ title }, "logged destructured title in POST method");
+  
+  const newNote = {
+    title,
+    text,
+    
+    id: "123456789"
+  }
+  
+  const returnedNote = addNewNote(newNote, notes);
+  
+ 
+  res.json(returnedNote);
+})
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);

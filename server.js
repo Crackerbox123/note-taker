c3ff5f1efc2e3857efa665b4c9a9ddfd45a04e8c
Kal O <koestreicher4@gmail.com>
2022-11-01 14:15:34 -0500

    uui and deploy test

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const express = require('express');
-const notes = require('./db/db.json'); 
+const notes = require('./db/db.json');
 
 const PORT = process.env.PORT || 3001;
 const path = require('path'); 
 const fs = require('fs'); 
 const { v4: uuidv4 } = require('uuid'); 
-const e = require('express');
 const app = express(); 
-
 
 app.use(express.static('public'));
 
@@ -16,30 +14,41 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); 
 
 
-const addNewNote = newNote => {
-  console.log(newNote, "body crossed over to NEW NOTE FUNC");
 
+app.post('/api/notes', (req, res) => {
+  const { title, text } = req.body;
+  
+  const newNote = {
+    title,
+    text,
+    
+    id: uuidv4()
+  }
+
+
+  notes.push(newNote);
+  
   
   fs.readFile('./db/db.json', 'utf8', (err, data) => {
     if (err) {
       console.log(err);
+      return res.sendStatus(500);
     }
     else {
       const parsedNotesArr = JSON.parse(data);
 
-      
+     
       parsedNotesArr.push(newNote);
 
-      
+     
       fs.writeFileSync(
         path.join(__dirname, './db/db.json'),
         JSON.stringify(parsedNotesArr, null, 2)
-      );
-      return parsedNotesArr;
+      )
     }
   });
-};
-
+  res.json(notes);
+});
 
 
 app.get('/api/notes', (req, res) => {
@@ -55,23 +64,6 @@ app.get('/notes', (req, res) => {
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
-
-
-
-app.post('/api/notes', (req, res) => {
-  const { title, text } = req.body;
-  
-  const newNote = {
-    title,
-    text,
-    
-    id: uuidv4()
-  }
-  
- 
-  const returnedNote = addNewNote(newNote);
-  res.json(returnedNote);
-})
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
